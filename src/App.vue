5c96fa36887dc9b75fddcead84731e4ea536d745
ovenzeze <ovenzeze@qq.com>
2017-02-17 23:03:49 +0800

    header组件|遮罩层

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ export default{
 };
 </script>
 
-<style lang="stylus">
+<style lang="stylus" rel="stylesheet/stylus">
   @import 'common/stylus/mixin.styl'
   .tab
     display: flex
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -32,14 +32,41 @@
   </div>
   <div v-show="detailShow" class="detail">
     <div class="detail-wrapper clear-fix">
-      <div class="detail-main"></div>
+      <div class="detail-main">
+        <h1 class="name">{{seller.name}}</h1>
+        <div class="star-wrapper">
+        <star :size="48" :score="seller.score"></star>
+        </div>
+        <div class="title">
+        <div class="line"></div>
+        <div class="text">优惠信息</div>
+        <div class="line"></div>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item" v-for="item in seller.supports">
+            <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+            <span class="text">{{seller.supports[$index].description}}</span>
+          </li>
+        </ul>
+        <div class="title">
+          <div class="line"></div>
+          <div class="text">商家公告</div>
+          <div class="line"></div>
+        </div>
+        <div class="bulletin">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+      </div>
     </div>
-    <div class="detail-close"></div>
+    <div class="detail-close">
+      <i class="icon-close"></i>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
+  import star from 'components/star/star';
 export default{
     props: {
         seller: {
@@ -58,7 +85,10 @@ export default{
     },
     created() {
         this.classMap = ["decrease","discount","special","invoice","guarantee"];
-  }
+    },
+    components: {
+        star: star
+    }
 };
 </script>
 
@@ -97,7 +127,6 @@ export default{
             line-height: 18px
             font-weight: bold
             vertical-align: top
-
         .description
           margin-bottom: 10px
           line-height: 12px
@@ -184,4 +213,76 @@ export default{
       height: 100%
       overflow: auto
       background: rgba(7,17,27,0.8)
+      .detail-wrapper
+        min-height: 100%
+        width: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            text-align: center
+            line-height: 16px
+            font-style: 16px
+            font-weight: 700
+          .star-wrapper
+            margin-top: 18px
+            padding: 2px 0
+            text-align: center
+          .title
+            display: flex
+            width: 80%
+            margin: 28px auto 24px auto
+            .line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(225,225,225,0.2)
+            .text
+              padding: 0 12px
+              font-weight: 700
+              font-size: 14px
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              &.last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_1')
+                &.discount
+                  bg-image('discount_1')
+                &.guarantee
+                  bg-image('guarantee_1')
+                &.invoice
+                  bg-image('invoice_1')
+                &.special
+                  bg-image('special_1')
+
+              .text
+                line-height: 12px
+                font-size: 12px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
